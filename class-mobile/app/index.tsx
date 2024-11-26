6e5bb6a15a197e83fb246238f22bb0d4dc3ae6ba
nwd0907 <nwd0907@newbizstart.com>
2024-11-26 14:49:05 +0900

    first commit

---
+++ class-mobile/app/index.tsx
@@ -0,0 +1,7 @@
+// [ Section01 ]
+// import HelloWorldPage from "@/components/section01/01-01-hello-world";
+import ReactNativeTagsPage from "@/components/section01/01-02-react-native-tags";
+
+export default function 나의시작화면() {
+    return <ReactNativeTagsPage />
+}
