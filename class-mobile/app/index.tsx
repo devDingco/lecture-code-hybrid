1fdf299e28d2c2a6656caa7b0110b0f083037b98
nwd0907 <nwd0907@newbizstart.com>
2024-11-26 18:26:11 +0900

    Section01: 01-04-webview-with-next

--- class-mobile/app/index.tsx
+++ class-mobile/app/index.tsx
@@ -1,7 +1,9 @@
 // [ Section01 ]
 // import HelloWorldPage from "@/components/section01/01-01-hello-world";
-import ReactNativeTagsPage from "@/components/section01/01-02-react-native-tags";
+// import ReactNativeTagsPage from "@/components/section01/01-02-react-native-tags";
+// import WebviewPage from "@/components/section01/01-03-webview";
+import WebviewWithNextPage from "@/components/section01/01-04-webview-with-next";
 
 export default function 나의시작화면() {
-    return <ReactNativeTagsPage />
+    return <WebviewWithNextPage />
 }
